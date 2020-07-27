b3c9174de85624d8eed9c51ef5b95a85ceaa2fc2
Adriana Borjas <aborjasmontiel@gmail.com>
2020-07-27 18:12:22 -0400

    about text

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -13,8 +13,7 @@ export const About = () => {
       <div className={styles.info}>
         <img src="/images/profile.jpg" className={styles.img}/>
         <div className={styles.obj}>
-          <h5 className={styles.subtitle}> Objective</h5>
-          <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet officia doloremque autem velit a pariatur in quod esse tempora laborum eaque soluta omnis odit eum, sequi inventore incidunt placeat alias. lore ipsum dolor, sit amet consectetur adipisicing elit. Sit molestiae quod explicabo voluptas error, quis facilis laudantium, sed ad impedit, nesciunt magni recusandae iste.</p>
+          <p className={styles.p}>Hello there! I'm an engineer and front-end developer with additional studies in graphic design. Based in Maracaibo, Venezuela.<br/> <br/>I studied petroleum engineering at the University of Zulia. Then I descovered the world of frontend development and web design with e-learning platforms like Platzi and Udemy. I am now applying my engineering and self-learning skills to enhance my knowledge.</p>
         </div>
       </div>
       <Button />
